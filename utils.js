6ad241467ccfe7ce10044cf8bf7ad6ad81624af8
s21arvesh <131661674+s21arvesh@users.noreply.github.com>
2026-02-10 16:38:51 +0530

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,10 @@
+function calculate(a, b) {
+  return a + b;
+}
+
+function calculateDuplicate(a, b) {
+  return a + b;
+}
+
+// ❌ Fake token
+const GITHUB_TOKEN = "ghp_1234567890abcdef1234567890abcdef";
